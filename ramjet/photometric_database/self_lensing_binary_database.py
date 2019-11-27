a9520ec8ed27195ab3c75d678c92a31441050320
golmschenk <golmschenk@gmail.com>
2019-11-27 13:26:38 -0600

    Add self lensing binary dataset downloading

--- ramjet/photometric_database/self_lensing_binary_database.py
+++ ramjet/photometric_database/self_lensing_binary_database.py
@@ -3,19 +3,22 @@ Code for a database of self lensing binary synthetic data overlaid on real TESS 
 """
 import tarfile
 import pandas as pd
+import requests
+import tensorflow as tf
 from pathlib import Path
 
-import tensorflow as tf
+from ramjet.photometric_database.tess_lightcurve_label_per_time_step_database import \
+    TessLightcurveLabelPerTimeStepDatabase
 
-from ramjet.photometric_database.lightcurve_label_per_time_step_database import LightcurveLabelPerTimeStepDatabase
 
-
-class SelfLensingBinaryDatabase(LightcurveLabelPerTimeStepDatabase):
+class SelfLensingBinaryDatabase(TessLightcurveLabelPerTimeStepDatabase):
     """
     A database of self lensing binary synthetic data overlaid on real TESS data.
     """
+
     def __init__(self, data_directory='data/self_lensing_binaries'):
         super().__init__(data_directory=data_directory)
+        self.synthetic_signals_directory: Path = Path(self.data_directory, 'synthetic_signals')
 
     def general_preprocessing(self, example_path_tensor: tf.Tensor) -> (tf.Tensor, tf.Tensor):
         """
@@ -55,7 +58,38 @@ class SelfLensingBinaryDatabase(LightcurveLabelPerTimeStepDatabase):
             synthetic_signal.to_feather(synthetic_signal_path)
             synthetic_signal_txt_path.unlink()  # Delete old file.
 
+    def download_and_prepare_database(self, number_of_negative_lightcurves_to_download=10000):
+        print('Clearing data directory...')
+        self.clear_data_directory()
+        print('Downloading synthetic signals.')
+        synthetic_signals_url = ('https://files.slack.com/files-pri/TKNTDRH5J-FQR2KN5BJ/download/' +
+                                 'trainingsetedgeon.tar.gz?pub_secret=81aa782027')
+        response = requests.get(synthetic_signals_url)
+        path_to_agnieszka_compressed_data = Path(self.data_directory, 'TrainingSetedgeon.tar.gz')
+        with open(path_to_agnieszka_compressed_data, 'wb') as compressed_file:
+            compressed_file.write(response.content)
+        self.convert_raw_synthetic_data_to_project_format()
+        print('Downloading TESS observation list...')
+        tess_observations = self.get_all_tess_time_series_observations()
+        single_sector_observations = self.get_single_sector_observations(tess_observations)
+        single_sector_observations = self.add_sector_column_based_on_single_sector_obs_id(single_sector_observations)
+        single_sector_observations['tic_id'] = single_sector_observations['target_name'].astype(int)
+        single_sector_observations = single_sector_observations.sample(frac=1, random_state=0)  # Shuffle
+        # Shorten product list obtaining. Twice as much *should* be plenty to find what we need.
+        single_sector_observations = single_sector_observations.head(number_of_negative_lightcurves_to_download * 2)
+        single_sector_data_products = self.get_product_list(single_sector_observations)
+        single_sector_data_products = self.add_tic_id_column_based_on_single_sector_obs_id(single_sector_data_products)
+        single_sector_data_products = single_sector_data_products.sample(frac=1, random_state=0)  # Shuffle
+        download_manifest = self.download_products(
+            single_sector_data_products.head(number_of_negative_lightcurves_to_download)
+        )
+        print(f'Moving lightcurves to {self.lightcurve_directory}...')
+        for file_path_string in download_manifest['Local Path']:
+            file_path = Path(file_path_string)
+            file_path.rename(self.lightcurve_directory.joinpath(file_path.name))
+        print('Database ready.')
+
 
 if __name__ == '__main__':
     database = SelfLensingBinaryDatabase()
-    database.convert_raw_synthetic_data_to_project_format()
+    database.download_and_prepare_database()
