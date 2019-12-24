fe811c6fbcde2c9a4a04118a4ac00336ce535347
Greg Olmschenk <golmschenk@gmail.com>
2019-12-24 12:31:35 -0600

    Setup infer script to work for the single label microlensing case

--- infer.py
+++ infer.py
@@ -1,33 +1,32 @@
 """Code for inference on the contents of a directory."""
+import pandas as pd
 import tensorflow as tf
 from pathlib import Path
 
 from ramjet.analysis.lightcurve_visualizer import plot_lightcurve
 from ramjet.analysis.model_loader import get_latest_log_directory
-from ramjet.models import ConvolutionalLstm
+from ramjet.models import ConvolutionalLstm, SimpleLightcurveCnn
+from ramjet.photometric_database.microlensing_label_per_example_database import MicrolensingLabelPerExampleDatabase
 from ramjet.photometric_database.toi_lightcurve_database import ToiLightcurveDatabase
 
-saved_log_directory = get_latest_log_directory('logs')  # Uses the latest log directory's model.
-# saved_log_directory = Path('logs/baseline YYYY-MM-DD-hh-mm-ss')  # Specifies a specific log directory's model to use.
+# saved_log_directory = get_latest_log_directory('logs')  # Uses the latest log directory's model.
+saved_log_directory = Path('logs/MOA microlensing, label per example, simple CNN 2019-12-11-23-41-01')
 
 print('Setting up dataset...')
-database = ToiLightcurveDatabase()
-database.obtain_meta_data_frame_for_available_lightcurves()
-example_paths = [str(database.lightcurve_directory.joinpath('tess2018319095959-s0005-0000000117979897-0125-s_lc.fits'))]
+database = MicrolensingLabelPerExampleDatabase()
 # Uncomment below to run the inference for all validation files.
-# example_paths = pd.read_csv(saved_log_directory.joinpath('validation.csv'), header=None)[0].values
+example_paths = pd.read_csv(saved_log_directory.joinpath('validation.csv'), header=None)[0].values
 
 print('Loading model...')
-model = ConvolutionalLstm()
+model = SimpleLightcurveCnn()
 model.load_weights(str(saved_log_directory.joinpath('model.ckpt')))
 
 print('Inferring and plotting...')
 for example_path in example_paths:
-    example, label = database.evaluation_preprocessing(tf.convert_to_tensor(example_path))
-    prediction = model.predict(tf.expand_dims(example, axis=0))[0]
-    fluxes, times = database.load_fluxes_and_times_from_fits_file(example_path)
-    label, prediction = database.inference_postprocessing(label, prediction, times.shape[0])
-    tic_id = database.get_tic_id_from_single_sector_obs_id(Path(example_path).stem)
-    sector = database.get_sector_from_single_sector_obs_id(Path(example_path).stem)
-    plot_title = f'TIC {tic_id} sector {sector}'
-    plot_lightcurve(times, fluxes, label, prediction, title=plot_title, save_path=f'{plot_title}.png')
+    lightcurve = pd.read_feather(example_path)
+    fluxes = lightcurve['flux'].values
+    times = lightcurve['HJD'].values
+    example = database.preprocess_and_augment_lightcurve(fluxes)
+    example_tensor = tf.convert_to_tensor(example)
+    prediction = model.predict(tf.expand_dims(example_tensor, axis=0))[0]
+    # plot_lightcurve(times, fluxes, title=example_path, save_path=f'{example_path}.png')
